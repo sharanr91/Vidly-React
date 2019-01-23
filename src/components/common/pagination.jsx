9950c390eb8f5c1c5674a662983a4821298c0dc9
sharanr91 <sharanr91@Dear.General.github.com>
2019-01-23 20:11:40 +0800

    Initial Commit

---
+++ src/components/common/pagination.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import _ from "lodash";
+import PropTypes from "prop-types";
+
+class Pagination extends Component {
+  state = {};
+  render() {
+    const { itemsCount, pageSize, currentPage, onPageChange } = this.props;
+    console.log(currentPage);
+
+    const pagesCount = Math.ceil(itemsCount / pageSize);
+    if (pagesCount === 1) return null;
+    const pages = _.range(1, pagesCount + 1);
+
+    return (
+      <nav>
+        <ul className="pagination">
+          {pages.map(page => (
+            <li
+              key={page}
+              className={
+                page === currentPage ? "page-item active" : "page-item"
+              }
+            >
+              <a className="page-link" onClick={() => onPageChange(page)}>
+                {page}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
+};
+
+export default Pagination;
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  render() {
+    return <h1>Log in</h1>;
+  }
+}
+
+export default LoginForm;
